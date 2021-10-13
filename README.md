519284da8d817a66b38502ae49298f63942d1b97
minhdao <minhdao@nvg.vn>
2021-10-13 14:25:11 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # selenium-java-cucumber-maven-POM
+
+1. POM
+2. Utilities
+3. Docker
+4. Jenkins
+5. APIs
+6. DB
+7. dashboard
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,34 +1,32 @@
 package stepDefinitions;
 
-
 import com.cucumber.listener.Reporter;
 import cucumber.TestContext;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import managers.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
-public class Hooks{
-	TestContext testContext;
-	
-	
-	
-	public Hooks(TestContext context) {
-		
-		testContext = context;
-	}
+public class Hooks {
+  TestContext testContext;
 
-	@Before
-	public void beforeScenario(Scenario scenario)
-	{
-		System.out.println("test context: "+ testContext);
-		//Reporter.assignAuthor("praveendandu");
-		
-	}
-	
-	@After
-	public void closeApplication()
-	{
-		testContext.getWebDriverManager().quitDriver();
-	}
+  public Hooks(TestContext context) {
 
+    testContext = context;
+  }
+
+  @Before
+  public void beforeScenario(Scenario scenario) {
+    System.out.println("test context: " + testContext);
+    // Reporter.assignAuthor("praveendandu");
+    //    WebDriverManager webDriverManager = new WebDriverManager();
+  }
+
+  @After
+  public void closeApplication(Scenario scenario) {
+    testContext.getWebDriverManager().quitDriver(scenario);
+  }
 }
