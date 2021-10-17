442c0133fbcb01c219cb17a1bc2163240207a93c
minhdao <minhdao@nvg.vn>
2021-10-17 14:30:55 +0700

    update

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,15 +7,15 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions
- (features="src/test/resources/functionalTests",
+// (features="src/test/resources/functionalTests",
 // (
 //    features = "src/test/resources/functionalTests/Login.feature",
 // (
 //    features = "src/test/resources/functionalTests/Purchase.feature",
-//(
-//    features = "src/test/resources/functionalTests/Booking.feature",
+(
+    features = "src/test/resources/functionalTests/Booking.feature",
     glue = {"stepDefinitions"},
-         tags = {"@debug"},
+//         tags = {"@debug"},
     plugin = {
       "pretty",
       "json:target/cucumber-reports/Cucumber.json",
